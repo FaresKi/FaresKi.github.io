f051b86c12d6330a0bc0c14eade74e18b8843342
Fares Kissoum <kissoumfares@gmail.com>
2022-06-20 00:29:23 +0200

    gotta find a way to go up and down

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import Terminal, { ColorMode, LineType } from "react-terminal-ui";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+const historyOfCommands = [];
+const historyOfResponses = [
+  {
+    type: LineType.Output,
+    value:
+      "Hi! You found my portfolio, please type help to find out which commands you can type 💻",
+  },
+];
+let cursor = 0;
 
-function App() {
-  const [terminalLineData, setTerminalLineData] = useState([
-    {
-      type: LineType.Output,
-      value:
-        "Hi! You found my portfolio, please type help to find out which commands you can type 💻",
-    },
-  ]);
+const useKeyPress = (targetKey) => {
+  const [keyPressed, setKeyPressed] = useState(false);
+  const upHandler = ({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(false);
+      // const last = historyOfCommands.length - 1;
+      // const chosenCommand =
+      //   historyOfCommands[last - cursor >= 0 ? last - cursor : 0];
+      // console.log("chosenCommand", chosenCommand);
+      // cursor++;
+      // // eslint-disable-next-line react-hooks/rules-of-hooks
+      // useState([chosenCommand]);
+    }
+  };
+  const downHandler = ({ key }) => {
+    if (key === targetKey) {
+      setKeyPressed(true);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("keydown", downHandler);
+    window.addEventListener("keyup", upHandler);
+    // Remove event listeners on cleanup
+    return () => {
+      window.removeEventListener("keydown", downHandler);
+      window.removeEventListener("keyup", upHandler);
+    };
+  }, []); // Empty array ensures that effect is only run on mount and unmount
+
+  return keyPressed;
+};
+const App = () => {
+  const [terminalLineData, setTerminalLineData] = useState(historyOfResponses);
+  const isUpPressed = useKeyPress("ArrowUp");
+  const isDownPressed = useKeyPress("ArrowDown");
   // Terminal has 100% width by default so it should usually be wrapped in a container div
   return (
     <div className="container">
@@ -17,31 +53,40 @@ function App() {
         colorMode={ColorMode.Dark}
         lineData={terminalLineData}
         onInput={(terminalInput) => {
-          console.log(`New terminal input received: '${terminalInput}'`);
+          cursor = 0;
+          historyOfResponses.push({
+            type: LineType.Input,
+            value: terminalInput,
+          });
+          historyOfCommands.push(terminalInput);
+          console.log("terminalInput", terminalInput);
+          console.log("ifUpPressed", isUpPressed);
+          console.log("ifDownPressed", isDownPressed);
           switch (terminalInput) {
             default:
-              setTerminalLineData([
-                {
-                  type: LineType.Output,
-                  value:
-                    'Please type "help" to find out which commands you could use',
-                },
-              ]);
+              historyOfResponses.push({
+                type: LineType.Output,
+                value:
+                  'Please type "help" to find out which commands you could use.',
+              });
+              setTerminalLineData(historyOfResponses);
               break;
             case "help":
-              setTerminalLineData([
-                {
-                  type: LineType.Output,
-                  value:
-                    'There are 3 different commands "profile" to talk about myself and who I am 👤, "experience" where I present my professional experiences 💼 \nand "school" where I present my academic path 🏫.',
-                },
-              ]);
+              historyOfResponses.push({
+                type: LineType.Output,
+                value:
+                  "There are 3 different commands: \n" +
+                  "- profile: to talk about myself and who I am 👤\n" +
+                  "- experience: where I present my professional experiences 💼\n" +
+                  "- school: where I present my academic path 🏫.",
+              });
+              setTerminalLineData(historyOfResponses);
               break;
           }
         }}
       />
     </div>
   );
-}
+};
 
 export default App;
