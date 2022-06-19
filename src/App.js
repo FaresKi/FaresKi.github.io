78dfb82a6faf1d742c651126728b16366e4c4227
Fares Kissoum <kissoumfares@gmail.com>
2022-06-19 22:42:56 +0200

    made some progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from "./logo.svg";
-import "./App.css";
 import Terminal, { ColorMode, LineType } from "react-terminal-ui";
 import { useState } from "react";
 
 function App() {
   const [terminalLineData, setTerminalLineData] = useState([
-    { type: LineType.Output, value: "Welcome to the React Terminal UI Demo!" },
-    { type: LineType.Input, value: "Some previous input received" },
+    {
+      type: LineType.Output,
+      value:
+        "Hi! You found my portfolio, please type help to find out which commands you can type 💻",
+    },
   ]);
   // Terminal has 100% width by default so it should usually be wrapped in a container div
   return (
     <div className="container">
       <Terminal
-        name="React Terminal Usage Example"
-        colorMode={ColorMode.Light}
+        name="Welcome to my portfolio! 👋🏽"
+        colorMode={ColorMode.Dark}
         lineData={terminalLineData}
-        onInput={(terminalInput) =>
-          console.log(`New terminal input received: '${terminalInput}'`)
-        }
+        onInput={(terminalInput) => {
+          console.log(`New terminal input received: '${terminalInput}'`);
+          switch (terminalInput) {
+            default:
+              setTerminalLineData([
+                {
+                  type: LineType.Output,
+                  value:
+                    'Please type "help" to find out which commands you could use',
+                },
+              ]);
+              break;
+            case "help":
+              setTerminalLineData([
+                {
+                  type: LineType.Output,
+                  value:
+                    'There are 3 different commands "profile" to talk about myself and who I am 👤, "experience" where I present my professional experiences 💼 \nand "school" where I present my academic path 🏫.',
+                },
+              ]);
+              break;
+          }
+        }}
       />
     </div>
   );
