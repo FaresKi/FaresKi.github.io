43d157964a9f4514e8911bbf02066d1e442476ca
Fares Kissoum <kissoumfares@gmail.com>
2022-07-15 14:47:48 +0200

    testing something

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+import axios from "axios";
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import "./App.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+const getRawData = (url) => {
+  axios
+    .get(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods":
+          "GET, POST, PATCH, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+getRawData("https://www.linkedin.com/in/fares-k/");
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
   <React.StrictMode>
     <App />
